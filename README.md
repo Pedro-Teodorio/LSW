4a819bec00658e2589b5194079b3927d9f34794c
Pedro Teodorio <79485773+Pedro-Teodorio@users.noreply.github.com>
2024-02-15 00:51:53 +0000

    Aula 02 e Exercicios

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# LSW
+# Aula 02
+
+# Porque estudar Javascript
+
+- Javascript é uma linguagem de programação interpreta poderosa, flexivel e rápida sendo bastante usada para o desenvolivmento de páginas web
+
+- O Javascript é conhecido principalamente como a linguagem da maioria dos navegadores
+
+# O que podemos fazer com Javascript
+
+- Interagir com usuario
+- Interagir com conteudo do documento, alterando prioridades
+- Operações matematicas
---
+++ js/script.js
@@ -0,0 +1,35 @@
+// const btnEvent = document.getElementById("btn-event");
+// Exercício 1
+// alert("Aguarde um momento para o carregamento do site");
+
+// Exercício 2
+// btnEvent.addEventListener("click",()=>{
+//     alert("Botão clicado");
+// })
+
+// Exercício 3
+// let num_1 = parseInt(prompt("Digite o primeiro número"));
+// let num_2 = parseInt(prompt("Digite o segundo número"));
+// let num_3 = parseInt(prompt("Digite o terceiro número"));
+
+// if (num_1 > num_2 && num_1 > num_3) {
+//   alert("O primeiro número é o maior");
+// } else if (num_2 > num_1 && num_2 > num_3) {
+//   alert("O segundo número é o maior");
+// } else if (num_3 > num_1 && num_3 > num_2) {
+//   alert("O terceiro número é o maior");
+// } else {
+//   alert("Os números são iguais");
+// }
+
+// Exercício 4
+// let idade = prompt("Digite a sua idade");
+// if (isNaN(idade)) {
+//   alert("O valor digitado não é um número");
+// } else {
+//   if (idade <= 18) {
+//     alert("Você é menor de idade");
+//   } else {
+//     alert("Você é maior de idade");
+//   }
+// }
