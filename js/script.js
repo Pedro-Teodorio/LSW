e2c44471f55518066baae688cf47475d7b4f56b2
Pedro Teodorio <79485773+Pedro-Teodorio@users.noreply.github.com>
2024-02-15 01:01:59 +0000

    Aula 02 e Exercicios

--- js/script.js
+++ js/script.js
@@ -8,14 +8,14 @@
 // })
 
 // Exercício 3
-let num_1 = parseInt(prompt("Digite o primeiro número"));
-let num_2 = parseInt(prompt("Digite o segundo número"));
-let num_3 = parseInt(prompt("Digite o terceiro número"));
+// let num_1 = Number(prompt("Digite o primeiro número"));
+// let num_2 = Number(prompt("Digite o segundo número"));
+// let num_3 = Number(prompt("Digite o terceiro número"));
 
-if (num_1 > num_2 && num_1 > num_3) alert("O primeiro número é o maior");
-else if (num_2 > num_1 && num_2 > num_3) alert("O segundo número é o maior");
-else if (num_3 > num_1 && num_3 > num_2) alert("O terceiro número é o maior");
-else alert("Os números são iguais");
+// if (num_1 > num_2 && num_1 > num_3) alert("O primeiro número é o maior");
+// else if (num_2 > num_1 && num_2 > num_3) alert("O segundo número é o maior");
+// else if (num_3 > num_1 && num_3 > num_2) alert("O terceiro número é o maior");
+// else alert("Os números são iguais");
 
 // Exercício 4
 // let idade = prompt("Digite a sua idade");
